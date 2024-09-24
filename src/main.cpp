71db4ca09c8c92397dc2987aa5d5b7fbb7e3282f
fengxun2017 <574912883@qq.com>
2024-09-24 23:04:38 +0800

    add code

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 #include "tcp_server.h"
 #include "event_loop.h"
 
+
+void new_conn_cb(tinynet::TcpConnection &conn)
+{
+    LOG(DEBUG) << "new conn:" << conn.get_name() << std::endl;
+}
+
 int main(void)
 {
-    tinynet::EventLoop loop;
-    tinynet::TcpServer tcp_server(&loop, "127.0.0.1", 14000, "test_server");
+    tinynet::EventLoop event_loop;
+    tinynet::TcpServer tcp_server(&event_loop, "127.0.0.1", 14000, "test_server");
 
     tcp_server.start();
+    tcp_server.set_newconn_cb(new_conn_cb);
+    event_loop.loop();
 
     return 0;
 }
