44d24682e1225353d55e38d7ab7a3ae43bcf070e
xun.feng1 <xun.feng1@nio.com>
2024-10-26 11:08:40 +0800

    add code

---
+++ src/http_server_test.cpp
@@ -0,0 +1,34 @@
+#include "http_server.h"
+#include "event_loop.h"
+#include "logging.h"
+
+void on_request(const tinynet::HttpRequest &request, tinynet::HttpResponse &response)
+{
+    LOG(DEBUG) << "recv URL:" <<request.get_url() << std::endl; 
+    LOG(DEBUG) << "recv headers:" << request.dump_heads() << std::endl;
+    LOG(DEBUG) << "recv body:" << request.get_body() << std::endl;
+    if (request.get_method() == tinynet::HttpRequest::GET)
+    {
+        response.set_body("Hello, world! GET");
+    }
+    else if(request.get_method() == tinynet::HttpRequest::POST)
+    {
+        response.set_body("Hello, world! POST");
+    }
+    else
+    {
+        response.set_status(501, "Not Implemented");
+        response.set_body("Unsupported method");
+    }
+}
+
+int main(void)
+{
+    tinynet::EventLoop event_loop;
+    tinynet::HttpServer server(&event_loop, "192.168.56.103", 8070, "http_test_server");
+    server.set_onrequest_cb(on_request);
+    server.start();
+    event_loop.loop();
+
+    return 0;
+}
